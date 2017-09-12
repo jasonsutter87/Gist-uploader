dd9dcc1e47828f84a3ddf02c6a9deb9ca8c8ffe9
Jason Sutter <jasonsutter87@gmail.com>
2017-09-12 10:45:13 -0500

    Implement readFile for a single argument

--- gistUploader.js
+++ gistUploader.js
@@ -1,8 +1,11 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
 
 http.createServer(function (req, res) {
   res.writeHead(200);
-  res.write('Hello World');
-  res.end();
+  fs.readFile(process.argv[2], function(error, contents) {
+    res.write(contents);
+    res.end();
+  });
 }).listen(8080);
 console.log('Listening on port 8080');
