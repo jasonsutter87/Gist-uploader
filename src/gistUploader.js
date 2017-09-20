2a810ac0297bd5005fc5263c518336271d3f453e
Jason Sutter <jasonsutter87@gmail.com>
2017-09-20 12:31:20 -0500

    Implement upload gist to users github account

--- src/gistUploader.js
+++ src/gistUploader.js
@@ -7,9 +7,12 @@ const GIST_URL = 'https://api.github.com/gists';
  * @param {string} file - The name of the file for the Gist.
  * @param {string} input - The contents of the Gist.
  * @param {string} description - The description of the Gist, Defaults to 'Gist description'.
+ * @param {string} username - The username of a GitHub user'.
+ * @param {string} usersPassword - The password of a GitHub user'.
+ * @param {boolean} isPublic - Sets if the gist is public or not'.
  * @returns {(string | error)} - String of the Gist URL or an error
  */
-async function uploadGist(file, content, description = 'Gist description') {
+async function uploadGist(file, content, description = 'Gist description', username='', usersPassword='', isPublic) {
   try {
     if (!file) {
       throw new Error('File not present');
@@ -21,18 +24,42 @@ async function uploadGist(file, content, description = 'Gist description') {
       throw new Error('Content not a string');
     }
 
-    const result = await axios.post(
-      GIST_URL,
-      {
-        'description': description,
-        'public': true,
-        'files': {
-          [file]:  {
-            'content': content,
+    let result;
+    if(username !== '' &&  usersPassword !== '') {
+      result = await axios({
+        method: 'post',
+        url: GIST_URL,
+        auth: {
+          username: username,
+          password: usersPassword
+        },
+        data: {
+          'description': description,
+          'public': isPublic,
+          'files': {
+            [file]:  {
+              'content': content,
+            },
           },
         },
-      },
-    );
+      }
+      );
+    } else {
+      result = await axios({
+        method: 'post',
+        url: GIST_URL,
+        data: {
+          'description': description,
+          'public': isPublic,
+          'files': {
+            [file]:  {
+              'content': content,
+            },
+          },
+        },
+      }
+      );
+    }
     process.stdout.write('\nGist has been uploaded\n')
     return result.data.html_url;
   } catch (error) {
--- src/runner.js
+++ src/runner.js
@@ -1,12 +1,22 @@
 const readFile = require('./readFile');
 const gistUploader = require('./gistUploader');
+const prompt = require('syncprompt');
 
 async function Runner() {
   try {
+    let username;
+    let usersPassword;
+    const isPublic = prompt("Is this a public gist? (true, false)\t");
+    const uploadToUserAccount = prompt("Upload the gist to users account? (yes, no)\t");
+    if(uploadToUserAccount.toLowerCase() === 'yes') {
+      username = prompt("What is your Github username?\t");
+      usersPassword = prompt("What is your Github password?\t", { secure: true });
+    }
+
     const fileName = process.argv[2];
     const gistDescription = process.argv[3];
     const contents = await readFile.readFile(fileName);
-    const upload = await gistUploader.uploadGist(fileName, contents, gistDescription);
+    const upload = await gistUploader.uploadGist(fileName, contents, gistDescription, username, usersPassword, isPublic);
     console.log(`Gist URL:  ${upload}`);
   } catch (error) {
     console.log(error);
