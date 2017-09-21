429a374174f97ed0373ff1a7ba35e1dcb8e326f3
Jason Sutter <jasonsutter87@gmail.com>
2017-09-21 14:29:28 -0500

    Update Gist Uploader

--- src/gistUploader.js
+++ src/gistUploader.js
@@ -5,39 +5,42 @@ const GIST_URL = 'https://api.github.com/gists';
 /**
  * Upload a Gist to Github.
  * @param {string} file - The name of the file for the Gist.
- * @param {string} input - The contents of the Gist.
+ * @param {string} content - The contents of the Gist.
  * @param {string} description - The description of the Gist, Defaults to 'Gist description'.
  * @returns {(string | error)} - String of the Gist URL or an error
  */
 async function uploadGist(file, content, description = 'Gist description') {
   try {
-    if (!file) {
-      throw new Error('File not present');
-    } else if ( typeof file !== 'string') {
-      throw new Error('File not a string');
-    }
-
-    if (!content || typeof content !== 'string') {
-      throw new Error('Content not a string');
-    }
-
-    const result = await axios.post(
-      GIST_URL,
-      {
-        'description': description,
-        'public': true,
-        'files': {
-          [file]:  {
-            'content': content,
-          },
+    const post = {
+      description: description,
+      public: true,
+      files: {
+        file:  {
+          'content': content,
         },
       },
-    );
-    process.stdout.write('\nGist has been uploaded\n')
-    return result.data.html_url;
+    }
+    const result = await axios({
+      method: 'post',
+      url: GIST_URL,
+      data: post
+    })
+    .catch((error) => {
+      if(error.request) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    });
+    return {status: result.status, message: result.data.html_url};
   } catch (error) {
-    return error;
+    return {status: error.response.status, message: error.response.data.message};
   }
 }
 
-module.exports = { uploadGist };
+module.exports = uploadGist;
--- src/test/gistUploader.test.js
+++ src/test/gistUploader.test.js
@@ -1,10 +1,10 @@
-const gistUploader = require('../gistUploader');
+const uploadGist = require('../gistUploader');
 const axios = require('axios');
 
 describe('When not given Gist content or a description', () => {
   it('Returns an error', () => {
     axios.post = jest.fn(() => Promise.resolve({ data: { html_url: 'url' } }));
-    gistUploader.uploadGist()
+    uploadGist()
       .then((result) => {
         expect(result).toEqual(new Error('File not present'));
       });
@@ -15,28 +15,10 @@ describe('When given Gist content and a description', () => {
   describe('When Gist inputs are a string', () => {
     it('Returns a Gist URL', () => {
       axios.post = jest.fn(() => Promise.resolve({ data: { html_url: 'url' } }));
-      gistUploader.uploadGist('test', 'string')
+      uploadGist('test', 'string')
         .then((result) => {
           expect(result).toEqual('url');
         });
       });
     });
-
-  describe('When one of the Gist inputs are not a string', () => {
-    it('Returns an error', () => {
-      axios.post = jest.fn(() => Promise.resolve({ data: { html_url: 'url' } }));
-      gistUploader.uploadGist('test', 343)
-        .then((result) => {
-          expect(result).toEqual(new Error('Content not a string'));
-        });
-    });
-
-    it('Returns an error', () => {
-      axios.post = jest.fn(() => Promise.resolve({ data: { html_url: 'url' } }));
-      gistUploader.uploadGist(24324, 'string')
-        .then((result) => {
-          expect(result).toEqual(new Error('File not a string'));
-        });
-      });
-  });
 });
